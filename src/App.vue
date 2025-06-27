5c45680ee93f35d82510bb0f519203c79d559194
puntodamar <punto@privyid.tech>
2025-06-28 01:06:46 +0700

    add: dynamic component

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,12 @@
   <div>
 
     <the-header></the-header>
+    <keep-alive>
+      <component :is="selectedComponent"></component>
+    </keep-alive>
+
+    <button @click="setSelectedComponent('active-goal')">Active Goal</button>
+    <button @click="setSelectedComponent('manage-goal')">Manage Goal</button>
     <badge-list></badge-list>
     <user-info
         :full-name="activeUser.name"
@@ -19,6 +25,8 @@ import BadgeList from './components/BadgeList.vue';
 import UserInfo from './components/UserInfo.vue';
 import TheHeader from './components/TheHeader.vue';
 import CourseGoal from "@/components/CourseGoal.vue";
+import ActiveGoal from "@/components/ActiveGoal.vue";
+import ManageGoal from "@/components/ManageGoal.vue";
 
 export default {
   components: {
@@ -26,9 +34,12 @@ export default {
     UserInfo,
     TheHeader,
     CourseGoal,
+    ActiveGoal,
+    ManageGoal,
   },
   data() {
     return {
+      selectedComponent: 'active-goal',
       activeUser: {
         name: 'John Doe',
         description: 'Site owner and admin',
@@ -36,6 +47,11 @@ export default {
       },
     };
   },
+  methods: {
+    setSelectedComponent(component) {
+      this.selectedComponent = component;
+    }
+  }
 };
 </script>
 
---
+++ src/components/ManageGoal.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h2>Manage Goal</h2>
+    <input type="text" ref="goal">
+    <button @click="setGoal">Set Goal</button>
+    <error-alert v-if="inputIsInvalid">
+      <h2>Input is invalid</h2>
+      <p>Please input a valid input</p>
+      <button @click="confirmError">Okey</button>
+    </error-alert>
+  </div>
+</template>
+
+<script>
+  import ErrorAlert from "@/components/ErrorAlert.vue";
+
+  export default {
+    components: {ErrorAlert},
+    data() {
+      return {
+        inputIsInvalid: false,
+      }
+    },
+    methods: {
+      setGoal() {
+        const value = this.$refs.goal.value;
+        if (value === '') {
+          this.inputIsInvalid = true;
+        }
+      },
+      confirmError() {
+        this.inputIsInvalid = false;
+      }
+    }
+  }
+</script>
